57105b35b761c5b4f934d57be14923bae186b72e
Pedro <pedroalencarregis@hotmail.com>
2020-06-14 17:39:46 -0300

    Initial try

---
+++ python-manylinux-demo.nix
@@ -0,0 +1,11 @@
+let
+  pkgs = import <nixpkgs> {};
+  mkDerivation = import ./autotools.nix pkgs;
+  version = "3.10.3";
+in mkDerivation {
+  name = "python-manylinux-demo";
+    src = pkgs.fetchurl {
+      url = "https://sourceforge.net/projects/math-atlas/files/Stable/${version}/atlas${version}.tar.bz2/download";
+      sha256 = "c510e3ad0200517e3a14534e494b37dc0770efd733fc35ce2f445dd49c96a7d5";
+    };
+}
