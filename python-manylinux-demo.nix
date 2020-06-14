38456d52c491772138b7a07c9261c70481620962
Pedro <pedroalencarregis@hotmail.com>
2020-06-14 18:09:16 -0300

    Change name

--- python-manylinux-demo.nix
+++ python-manylinux-demo.nix
@@ -3,7 +3,7 @@ let
   mkDerivation = import ./autotools.nix pkgs;
   version = "3.10.3";
 in mkDerivation {
-  name = "python-manylinux-demo";
+  name = "atlas";
     src = pkgs.fetchurl {
       url = "https://sourceforge.net/projects/math-atlas/files/Stable/${version}/atlas${version}.tar.bz2/download";
       sha256 = "c510e3ad0200517e3a14534e494b37dc0770efd733fc35ce2f445dd49c96a7d5";
